ab30d08b5483ac52a2a2f6ba43dd5b1e0bc2d071
Frankoropeza <frankoropeza840@gmail.com>
2026-04-03 18:07:07 -0600

    fix: update site URL to turbo-racing.mx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://turboracing.com',
+  site: 'https://turbo-racing.mx',
   integrations: [mdx(), sitemap(), tailwind()],
   image: {
     service: {
